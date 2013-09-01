0cdd0f530e3e20893d1b7cf88e0f6239938dd602
Clement Escoffier <clement.escoffier@gmail.com>
2013-09-01 15:36:01 +0200

    Fix issue #5
    Added a 'start' mojo.
    System properties from the command line are copied, except the default properties set by the JVM
    
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

--- src/main/java/org/nanoko/playframework/mojo/AbstractPlay2Mojo.java
+++ src/main/java/org/nanoko/playframework/mojo/AbstractPlay2Mojo.java
@@ -15,7 +15,6 @@
 package org.nanoko.playframework.mojo;
 
 
-
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -23,15 +22,85 @@ import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.MavenProjectHelper;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
-import java.util.HashSet;
+import java.util.*;
+
 /**
  * Common parent of all Play 2 Mojo
  */
 public abstract class AbstractPlay2Mojo extends AbstractMojo {
+
+    public static final String ENV_PLAY2_HOME = "PLAY2_HOME";
+    public static final String PLAY2_ARG_FORMAT = "-D%s=%s";
+    /**
+     * The list of system properties excluded when copying them to the `play` command line.
+     */
+    private static final List<String> EXCLUDED_PROPERTIES = Arrays.asList(
+            "java.runtime.name",
+            "sun.boot.library.path",
+            "java.vm.version",
+            "user.country.format",
+            "com.apple.mrj.application.apple.menu.about.name",
+            "java.vm.vendor",
+            "java.vendor.url",
+            "path.separator",
+            "java.vm.name",
+            "file.encoding.pkg",
+            "user.country",
+            "sun.java.launcher",
+            "sun.os.patch.level",
+            "java.vm.specification.name",
+            "user.dir",
+            "java.runtime.version",
+            "java.awt.graphicsenv",
+            "java.endorsed.dirs",
+            "os.arch",
+            "java.io.tmpdir",
+            "line.separator",
+            "java.vm.specification.vendor",
+            "os.name",
+            "tools.jar",
+            "sun.jnu.encoding",
+            "script.name",
+            "java.library.path",
+            "sun.awt.enableExtraMouseButtons",
+            "java.specification.name",
+            "java.class.version",
+            "sun.management.compiler",
+            "os.version",
+            "http.nonProxyHosts",
+            "user.home",
+            "user.timezone",
+            "java.awt.printerjob",
+            "file.encoding",
+            "java.specification.version",
+            "java.class.path",
+            "user.name",
+            "java.vm.specification.version",
+            "sun.java.command",
+            "java.home",
+            "sun.arch.data.model",
+            "user.language",
+            "java.specification.vendor",
+            "user.language.format",
+            "awt.toolkit",
+            "java.vm.info",
+            "java.version",
+            "java.ext.dirs",
+            "sun.boot.class.path",
+            "java.vendor",
+            "file.separator",
+            "java.vendor.url.bug",
+            "sun.font.fontmanager",
+            "sun.io.unicode.encoding",
+            "sun.cpu.endian",
+            "socksNonProxyHosts",
+            "ftp.nonProxyHosts",
+            "sun.cpu.isalist",
+            // List related to Maven source
+            "maven.home",
+            "guice.disable.misplaced.annotation.check",
+            "classworlds.conf"
+    );
 
     /**
      * The maven project.
@@ -41,7 +110,6 @@ public abstract class AbstractPlay2Mojo extends AbstractMojo {
      * @readonly
      */
     MavenProject project;
-
     /**
      * The maven session.
      *
@@ -50,19 +118,18 @@ public abstract class AbstractPlay2Mojo extends AbstractMojo {
      * @readonly
      */
     MavenSession session;
-
     /**
      * Directory containing the build files.
+     *
      * @parameter expression="${project.build.directory}"
      */
     File buildDirectory;
-
     /**
      * Base directory of the project.
+     *
      * @parameter expression="${basedir}"
      */
     File baseDirectory;
-
     /**
      * Maven ProjectHelper.
      *
@@ -70,7 +137,6 @@ public abstract class AbstractPlay2Mojo extends AbstractMojo {
      * @readonly
      */
     MavenProjectHelper projectHelper;
-
     /**
      * The PLAY2_HOME path is taken from this setting, if not found as a Java system property (-DPLAY2_HOME).
      * Refers to the PLAY2_HOME environment variable by default.
@@ -85,30 +151,35 @@ public abstract class AbstractPlay2Mojo extends AbstractMojo {
      * @parameter expression="${env.PLAY2_HOME}"
      */
     String play2Home;
-
     /**
      * Sets a timeout to the <tt>play</tt> invocation (in milliseconds).
      * If not set (or set to <tt>-1</tt>, the plugin waits until the underlying <tt>play</tt> process completes.
      * If set, the plugin kills the underlying <tt>play</tt> process when the timeout is reached, and it fails the build.
+     *
      * @parameter default-value="-1" expression="${play2timeout}"
      */
     long timeout;
-
-
-    public static final String ENV_PLAY2_HOME = "PLAY2_HOME";
-
-    public static final String PLAY2_ARG_FORMAT = "-D%s=%s";
-
+    /**
+     * Allows customization of the play execution System properties.
+     *
+     * @parameter
+     */
+    Properties play2SystemProperties = new Properties();
     /**
      * Stored the play 2 executable once found to avoid multiple searches.
      */
     private File play2executable;
 
     /**
-     * Allows customization of the play execution System properties.
-     * @parameter
+     * Checks whether the current operating system is Windows.
+     * This check use the <tt>os.name</tt> system property.
+     *
+     * @return <code>true</code> if the os is windows, <code>false</code> otherwise.
      */
-    Properties play2SystemProperties = new Properties();
+    /* package */
+    static boolean isWindows() {
+        return System.getProperty("os.name").toLowerCase().contains("win");
+    }
 
     /**
      * Gets the specified <tt>PLAY2_HOME</tt> location.
@@ -190,15 +261,16 @@ public abstract class AbstractPlay2Mojo extends AbstractMojo {
     /**
      * Checks whether the given play executable is in a <tt>Homebrew</tt> managed location.
      * Homebrew scripts seems to be an issue for play as the provided play executable from this directory is using a
-     *  path expecting relative directories. So, we get such kind of error:
-     *  <br/>
+     * path expecting relative directories. So, we get such kind of error:
+     * <br/>
      * <code>
-     *    /usr/local/Cellar/play/2.0/libexec/play: line 51:
-     *    /usr/local/Cellar/play/2.0/libexec//usr/local/Cellar/play/2.0/libexec/../libexec/framework/build:
-     *    No such file or directory
+     * /usr/local/Cellar/play/2.0/libexec/play: line 51:
+     * /usr/local/Cellar/play/2.0/libexec//usr/local/Cellar/play/2.0/libexec/../libexec/framework/build:
+     * No such file or directory
      * </code>
      * <br/>
      * In this case we substitute the play executable with the one installed by Homebrew but working correctly.
+     *
      * @param play2 the found play2 executable
      * @return the given play2 executable except if Homebrew is detected, in this case <tt>/usr/local/bin/play</tt>.
      */
@@ -207,7 +279,7 @@ public abstract class AbstractPlay2Mojo extends AbstractMojo {
             getLog().info("Homebrew installation of play detected");
             // Substitute the play executable by the homebrew one.
             File file = new File("/usr/local/bin/play");
-            if (! file.exists()) {
+            if (!file.exists()) {
                 getLog().error("Homebrew installation detected, but no play executable in /usr/local/bin");
             } else {
                 return file;
@@ -241,26 +313,17 @@ public abstract class AbstractPlay2Mojo extends AbstractMojo {
     }
 
     /**
-     * Checks whether the current operating system is Windows.
-     * This check use the <tt>os.name</tt> system property.
-     *
-     * @return <code>true</code> if the os is windows, <code>false</code> otherwise.
-     */
-    /* package */ static boolean isWindows() {
-        return System.getProperty("os.name").toLowerCase().contains("win");
-    }
-
-    /**
      * Gets the execution environment.
      * This method builds a map containing the Maven properties to give to SBT invocations.
      * It contains maven project data (GAV), pom properties, and system properties.
+     *
      * @return the map of properties (<code><pre>key -> value</pre></code>)
      */
     public Map<String, String> getEnvironment() {
         Map<String, String> env = new HashMap<String, String>();
-        
+
         // Environment variables
-        env.putAll( System.getenv() );
+        env.putAll(System.getenv());
 
         // Build properties.
         env.put("project.groupId", project.getGroupId());
@@ -280,7 +343,7 @@ public abstract class AbstractPlay2Mojo extends AbstractMojo {
         for (String k : environment.keySet()) {
             env.put(k, environment.get(k));
         }
-        
+
         // System properties
         props = System.getProperties();
         if (props != null) {
@@ -288,7 +351,7 @@ public abstract class AbstractPlay2Mojo extends AbstractMojo {
                 env.put(entry.getKey().toString(), entry.getValue().toString());
             }
         }
-        
+
         getLog().debug("Environment built : " + env);
         return env;
     }
@@ -316,23 +379,39 @@ public abstract class AbstractPlay2Mojo extends AbstractMojo {
         for (Map.Entry<Object, Object> entry : play2SystemProperties.entrySet()) {
             args.add(String.format(PLAY2_ARG_FORMAT, entry.getKey(), entry.getValue()));
         }
-        manageProxySettings(args);
-
+        addProxiesSystemProperties(args);
+        appendCurrentSystemProperties(args);
 
         return args.toArray(new String[0]);
+    }
+
+    /**
+     * Iterates over the system properties and append them to the list of system properties.
+     * Some default system properties are excluded.
+     *
+     * @param system the system properties.
+     */
+    private void appendCurrentSystemProperties(Set<String> system) {
+        Properties properties = System.getProperties();
+        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+            if (!EXCLUDED_PROPERTIES.contains(entry.getKey())) {
+                system.add(String.format(PLAY2_ARG_FORMAT, entry.getKey(), entry.getValue()));
+            }
+        }
     }
 
     /**
      * Appends the proxy settings (extracted from the Maven settings) to the system properties list.
      * It adds the following properties:
      * <code>
-     *     <pre>
+     * <pre>
      *        -Dhttp.proxyHost=yourserver -Dhttp.proxyPort=8080 -Dhttp.proxyUser=username -Dhttp.proxyPassword=password
      *     </pre>
      * </code>
+     *
      * @param system the system properties.
      */
-    private void manageProxySettings(Set<String> system) {
+    private void addProxiesSystemProperties(Set<String> system) {
         // Skip if there is no proxy.
         if (session.getSettings().getActiveProxy() == null) {
             return;
@@ -343,7 +422,7 @@ public abstract class AbstractPlay2Mojo extends AbstractMojo {
         int proxyPort = session.getSettings().getActiveProxy().getPort();
         String proxyUser = session.getSettings().getActiveProxy().getUsername();
         String proxyPassword = session.getSettings().getActiveProxy().getPassword();
-        if ("http".equals(proxyProtocol)  && proxyHost != null) {
+        if ("http".equals(proxyProtocol) && proxyHost != null) {
             system.add(String.format(PLAY2_ARG_FORMAT, "http.proxyHost", proxyHost));
             if (proxyPort != 0) {
                 system.add(String.format(PLAY2_ARG_FORMAT, "http.proxyPort", proxyPort));
---
+++ src/main/java/org/nanoko/playframework/mojo/Play2StartMojo.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2013 OW2 Nanoko Project
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.nanoko.playframework.mojo;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import java.io.IOException;
+
+/**
+ * Starts the Play application in production mode.
+ * We aware that to leave the started application, you must kill the process.
+ *
+ * @goal start
+ * @requiresProject false
+ * @requiresDependencyResolution provided
+ *
+ */
+public class Play2StartMojo
+        extends AbstractPlay2Mojo {
+
+    public void execute()
+            throws MojoExecutionException {
+
+        String line = getPlay2().getAbsolutePath();
+
+        CommandLine cmdLine = CommandLine.parse(line);
+        cmdLine.addArguments(getPlay2SystemPropertiesArguments(), false);
+        cmdLine.addArgument("start");
+        System.out.println(cmdLine.toString());
+        DefaultExecutor executor = new DefaultExecutor();
+
+        // As where not linked to a project, we can't set the working directory.
+        // So it will use the directory where mvn was launched.
+
+        executor.setExitValue(0);
+        try {
+            executor.execute(cmdLine, getEnvironment());
+        } catch (IOException e) {
+            // Ignore.
+        }
+    }
+}
